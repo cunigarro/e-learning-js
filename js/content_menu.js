2aceada238771d7a8d8d284dd83e2fe99364312b
Christian Unigarro <cunigarrod@gmail.com>
2018-02-05 23:27:58 -0500

    Update list on navigation menu based on changes.

--- js/content_menu.js
+++ js/content_menu.js
@@ -10,6 +10,5 @@ var content_menu =
     '<li><a href="p08.html" target="_self">7. Drag and drop images</a></li>' +
     '<li><a href="p09.html" target="_self">8. Accordion select</a></li>' +
     '<li><a href="p10.html" target="_self">9. Crossword puzzle</a></li>' +
-    '<li><a href="p11.html" target="_self">10. Concentrate</a></li>' +
-    '<li><a href="p12.html" target="_self">11. Match up</a></li>' +
+    '<li><a href="p11.html" target="_self">10. Concentrate</a></li>'
 '</ul>';
