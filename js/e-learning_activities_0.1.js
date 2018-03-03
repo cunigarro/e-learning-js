9e526c598731ef0b698681203988b32b8eb17cf1
Christian Unigarro <cunigarrod@gmail.com>
2018-03-03 03:10:43 -0500

    WIP: Remove jquery dependency for select activity. (#1)

--- js/e-learning_activities_0.1.js
+++ js/e-learning_activities_0.1.js
@@ -91,125 +91,78 @@ function create_select(options) {
   var reset_button = document.querySelector(options.reset_button);
   var answer_button = document.querySelector(options.answer_button);
   var answers = options.answers;
+  window['selected'] = [];
 
   inputs.forEach(function(item, i) {
     var options = item.querySelectorAll('div');
-    options.forEach(function(item, i) {
+    var question = '';
+    var answer = '';
+
+    window['selected'].push({
+      question: i + 1,
+      answer: 0
+    });
+
+    options.forEach(function(item, k) {
       addEventHandler(item, 'click', function() {
+        question = this.parentNode.getAttribute('data-question');
+        answer = this.getAttribute('data-answer');
+
         Array.prototype.map.call(options, function(el) {
           el.classList.remove('selected');
         });
         this.classList.add('selected');
+        selected[question - 1].answer = answer;
       });
     });
 
     options[options.length - 1].insertAdjacentHTML('afterend', '<span></span>');
   });
 
-  // questions_html.on('click', 'div', function () {
-  //   $(this).parent().find('div').not($(this)).removeClass('selected');
-  //   $(this).addClass('selected');
+  addEventHandler(check_button, 'click', function() {
+    inputs.forEach(function(item, i) {
+      if(selected[i].answer != 0) {
+        if(selected[i].answer == answers[i]) {
+          item.children[item.children.length - 1].appendChild(addValidationIcon('good'));
+        } else {
+          item.children[item.children.length - 1].appendChild(addValidationIcon('wrong'));
+        }
 
-  //   answered = true;
-  //   var selected_option = $(this).data('select');
-  //   var question = questions[$(this).closest('li').data('question')];
-  //   question['answer'] = selected_option;
-  // });
+        answer_button.style.display = 'inline-block';
+        reset_button.style.display = 'inline-block';
+        check_button.style.display = 'none';
+      }
+    });
+  });
+
+  addEventHandler(reset_button, 'click', function() {
+    inputs.forEach(function(item, i) {
+      selected[i].answer = 0;
+      item.children[item.children.length - 1].innerHTML = '';
+      for(var k = 0; k < item.children.length; k++) {
+        item.children[k].classList.remove('selected');
+      };
+    });
+
+    answer_button.style.display = 'none';
+    reset_button.style.display = 'none';
+    check_button.style.display = 'inline-block';
+  });
+
+  addEventHandler(answer_button, 'click', function() {
+    inputs.forEach(function(item, i) {
+      item.children[item.children.length - 1].innerHTML = '';
+
+      for(var k = 0; k < item.children.length; k++) {
+        item.children[k].classList.remove('selected');
+      };
+
+      if(selected[i].answer != 0) {
+        item.children[answers[i] - 1].classList.add('selected');
+      }
+    });
+  });
 }
-/* function create_select(questions_html,check_button,reset_button,answer_button,answers_amount) {
-    answer_button.hide();
-    reset_button.hide();
-    check_button.css('position','relative');
-
-    if(questions_html.find('div').html().length == 1){
-        questions_html.find('div').css({
-            'width': 21,
-            'padding': 1
-        });
-    }else if(questions_html.find('figure').length){
-        questions_html.find('div').css('padding',0);
-    }
-
-    var answered = false;
-
-    var questions = JSON.parse(JSON.stringify(answers_amount));
-
-    for (qu in questions) {
-        questions[qu] = {};
-    }
-
-    var answers = answers_amount;
-
-    for (qu in questions) {
-        questions[qu]['answer'] = 0;
-    }
-
-    questions_html.on('click', 'div', function () {
-        $(this).parent().find('div').not($(this)).removeClass('selected');
-        $(this).addClass('selected');
-
-        answered = true;
-        var selected_option = $(this).data('select');
-        var question = questions[$(this).closest('li').data('question')];
-        question['answer'] = selected_option;
-    });
-
-    check_button.click(function () {
-        questions_html.find('li').each(function( key, element) {
-            var correct_answer = answers[$(element).data('question')];
-            var selected_answer = questions[$(element).data('question')]['answer'];
-
-            if (selected_answer) {
-                $(element).find('.good_icon').parent().addClass('activity_span');
-                if (correct_answer == selected_answer) {
-                    $(element).find('.good_icon').parent().fadeIn('normal').css("display","inline-block");
-                    $(element).find('.good_icon').fadeIn('normal').css("display","inline-block");
-                    $(element).find('.wrong_icon').css('display','none');
-                } else {
-                    $(element).find('.wrong_icon').parent().fadeIn('normal').css("display","inline-block");
-                    $(element).find('.wrong_icon').fadeIn('normal').css("display","inline-block");
-                    $(element).find('.good_icon').css('display','none');
-                }
-            }
-        });
-
-        if(answered === true){
-            answer_button.fadeIn();
-            check_button.css('display', 'none');
-            reset_button.fadeIn();
-        }
-    });
-
-    reset_button.click(function () {
-        answered = false;
-        for (qu in questions) {
-            questions[qu]['answer'] = 0;
-        }
-        questions_html.find('.good_icon, .wrong_icon').parent().fadeOut("normal");
-        questions_html.find('div').each(function( key, element) {
-            $(element).removeClass('selected');
-            $(element).removeClass('answer');
-        });
-
-        answer_button.hide();
-        check_button.fadeIn();
-        $(this).hide();
-    });
-
-    answer_button.click(function () {
-        questions_html.find('.good_icon, .wrong_icon').parent().fadeOut("normal");
-        questions_html.find('div').removeClass('selected');
-
-        for (question in questions) {
-            var correct_answer = answers[question];
-            var user_answer = questions[question]['answer'];
-
-            if (user_answer > 0) {
-                questions_html.find("[data-question='" + question + "']").find("[data-select='" + correct_answer + "']").addClass('answer');
-            }
-        }
-    });
-} */
 /*--Select--*/
 
 /*--Drag and drop--*/
