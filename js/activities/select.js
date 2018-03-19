d13983b96184b87fa99ede11676594066bb761a6
Christian Unigarro <cunigarrod@gmail.com>
2018-03-19 18:58:08 -0500

    WIP: Remove not necessary html and javascript (#1).

--- js/activities/select.js
+++ js/activities/select.js
@@ -6,37 +6,3 @@ create_select({
     answer_button: '#answers_3_1',
     answers: [3, 4, 2, 3, 2, 1, 3, 1, 4, 2]
 });
-
-/*--Page 3 activity 2--*/
-/* create_select (
-    $('.questions_3_2'),
-    $('#check_answers_3_2'),
-    $('#reset_answers_3_2'),
-    $('#answers_3_2'),
-    {
-        'question_3_2_1': '3',
-        'question_3_2_2': '4',
-        'question_3_2_3': '2',
-        'question_3_2_4': '3',
-        'question_3_2_5': '2',
-        'question_3_2_6': '1',
-        'question_3_2_7': '3',
-        'question_3_2_8': '1',
-        'question_3_2_9': '4',
-        'question_3_2_10': '2'
-    }
-); */
-
-/*--Page 3 activity 3--*/
-/* create_select (
-    $('.questions_3_3'),
-    $('#check_answers_3_3'),
-    $('#reset_answers_3_3'),
-    $('#answers_3_3'),
-    {
-        'question_3_3_1': '3',
-        'question_3_3_2': '1',
-        'question_3_3_3': '2',
-        'question_3_3_4': '3'
-    }
-); */
