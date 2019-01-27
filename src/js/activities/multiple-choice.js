fc1f9f5534ce51b297fd09b0f5c687b0b21dc469
Christian Unigarro <cunigarrod@gmail.com>
2019-01-26 21:49:31 -0500

    WIP: Validation forms added to multiple choice activity #11.

--- src/js/activities/multiple-choice.js
+++ src/js/activities/multiple-choice.js
@@ -7,11 +7,15 @@ const createMultipleChoice = (options) => {
   var answer_button = document.querySelector(options.answer_button);
   var answers = options.answers;
 
+  questions_html.insertAdjacentHTML('beforeend', '<span class="js-invalid-msg invalid_msg"></span>');
+
   inputs.forEach(function(item, i){
     item.insertAdjacentHTML('afterend', '<span></span>');
   });
 
   addEventHandler(check_button, 'click', function() {
+    var iterate = 0;
+
     inputs.forEach(function(item, i) {
       var correct_answer = answers[i];
       var user_answer = item.value;
@@ -27,8 +31,16 @@ const createMultipleChoice = (options) => {
         answer_button.style.display = 'inline-block';
         reset_button.style.display = 'inline-block';
         check_button.style.display = 'none';
+      } else {
+        iterate++;
       }
     });
+
+    if(iterate === inputs.length) {
+      questions_html.querySelector('.js-invalid-msg').innerHTML = 'Enter at least one response.';
+    } else {
+      questions_html.querySelector('.js-invalid-msg').innerHTML = '';
+    }
   });
 
   addEventHandler(reset_button, 'click', function() {
