8e7e4eaf2d6685ac5ab20d3c626bc310352261bf
Christian Unigarro <cunigarrod@gmail.com>
2025-06-30 17:57:15 -0500

    feat: add initial angular and react projects. #18

---
+++ angular-app/src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    
+  ]
+};
---
+++ angular-app/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'angular-app';
+}
